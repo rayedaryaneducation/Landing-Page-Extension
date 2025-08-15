bd5df538fbfc194234120ec51883328a357534e8
Aryan <rayed.aryan.00@education.tas.edu.au>
2025-08-15 14:16:55 +1000

    Connected API, implemented time an search fields, fixed layout

---
+++ index.js
@@ -0,0 +1,34 @@
+const accessKey = 'G3h4jjOce324fBCFgl2ozboTCTzr30cJ3lVQwl_5c4E';
+const bodyImage = document.getElementById('myElement')
+const userSearch = document.getElementById('user-search')
+const defaultImage = ``
+const timeDiv = document.getElementById('time-h1')
+function render(){
+    try{
+        fetch(`https://api.unsplash.com/photos/random?query=${userSearch.value}&client_id=${accessKey}`)
+            .then(res => res.json())
+            .then(data => bodyImage.style.backgroundImage = `url('${data.urls.full}')`)
+    } catch (error) {
+        fetch(`https://api.unsplash.com/photos/random?query=nature&client_id=${accessKey}`)
+            .then(res => res.json())
+            .then(data => bodyImage.style.backgroundImage = `url('${data.urls.full}')`)
+    }
+}
+render()
+function updateTime() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0'); 
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  timeDiv.textContent = `${hours}:${minutes}`;
+}
+updateTime(); 
+setInterval(updateTime, 1000);
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    let userSearchHist = userSearch.value
+    render()
+    userSearch.value =''
+  }
+});
+
+
