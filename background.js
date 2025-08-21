6e4475032040189ecba3f9769294fe53c4048c90
Aryan <rayed.aryan.00@education.tas.edu.au>
2025-08-21 11:06:26 +1000

    fix url

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onStartup.addListener(() => {
   // Use `chrome.runtime.getURL` to get the full URL of your extension page.
-  const pageUrl = chrome.runtime.getURL("landing_page.html");
+  const pageUrl = chrome.runtime.getURL("index.html");
 
   // Create a new tab with your extension's landing page.
   chrome.tabs.create({ url: pageUrl });
