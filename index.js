9110ec08618f9635f51a81a2ea3ad5270756114a
Rayed Aryan <rayed.aryan.00@education.tas.edu.au>
2025-08-28 10:54:04 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 chrome.windows.onCreated.addListener(function() {
   chrome.tabs.create({url:"index.html"});
 })
-function setFocusToTextBox(){
-    document.getElementById("mytext").focus();
-}
+
 const accessKey = 'G3h4jjOce324fBCFgl2ozboTCTzr30cJ3lVQwl_5c4E';
 const bodyImage = document.getElementById('myElement')
 const userSearch = document.getElementById('user-search')
 const defaultImage = ``
 const timeDiv = document.getElementById('time-h1')
+function setFocusToTextBox(){
+    userSearch.focus();
+}
 function render(){
     try{
         fetch(`https://api.unsplash.com/photos/random?query=&client_id=${accessKey}`)
