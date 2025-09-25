3cda5befd358072d058df52bcc7da7f8887bcd8f
rayedaryaneducation <rayed.aryan.00@education.tas.edu.au>
2025-09-25 10:52:22 +1000

    blah

--- index.js
+++ index.js
@@ -2,14 +2,31 @@ chrome.windows.onCreated.addListener(function() {
   chrome.tabs.create({url:"index.html"});
 })
 
+<<<<<<< HEAD
 async function render(){
     try{
         const res = await fetch(`https://api.unsplash.com/photos/random?query=&client_id=G3h4jjOce324fBCFgl2ozboTCTzr30cJ3lVQwl_5c4E`)
         const data = await res.json()
         document.getElementById('myElement').style.backgroundImage = `url('${data.urls.full}')`
+=======
+const accessKey = 'G3h4jjOce324fBCFgl2ozboTCTzr30cJ3lVQwl_5c4E';
+const bodyImage = document.getElementById('myElement')
+const userSearch = document.getElementById('user-search')
+const defaultImage = ``
+const timeDiv = document.getElementById('time-h1')
+
+function render(){
+    try{
+        fetch(`https://api.unsplash.com/photos/random?query=&client_id=${accessKey}`)
+            .then(res => res.json())
+            .then(data => bodyImage.style.backgroundImage = `url('${data.urls.full}')`)
+>>>>>>> parent of a2a0cea (Add updated files)
     } catch (error) {
-      console.log(error)
+        fetch(`https://api.unsplash.com/photos/random?query=nature&client_id=${accessKey}`)
+            .then(res => res.json())
+            .then(data => bodyImage.style.backgroundImage = `url('${data.urls.full}')`)
     }
+<<<<<<< HEAD
 }
 
 function updateTime() {
@@ -177,3 +194,23 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 render()
+=======
+}
+render()
+function updateTime() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0'); 
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  timeDiv.textContent = `${hours}:${minutes}`;
+}
+updateTime(); 
+setInterval(updateTime, 1000);
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter'){
+    window.open(`https://www.google.com/search?query=${userSearch.value}`, '_blank');
+    userSearch.value =  ''
+  }
+});
+
+
+>>>>>>> parent of a2a0cea (Add updated files)
